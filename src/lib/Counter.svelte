b5e7f2a615e1b66077945b95271f95d34461ba4c
hauselin <hauselin@gmail.com>
2023-01-15 18:42:20 -0500

    Add date

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,6 +1,6 @@
 <script>
   let today = new Date();
-  let die = new Date(2049, 12, 21); // 60 years from birthday
+  let date60 = new Date(2049, 12, 21); // 60 years from birthday
   let nextYear = new Date(today.getFullYear() + 1, 0, 1);
   let nextSunday = new Date(
     today.getFullYear(),
@@ -8,21 +8,27 @@
     today.getDate() + ((7 - today.getDay()) % 7)
   );
 
-  const days = (date1, date2) => {
-    let difference = date1.getTime() - date2.getTime();
-    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
-    return TotalDays;
+  const days = (end, start) => {
+    let difference = end.getTime() - start.getTime();
+    let diffDays = Math.ceil(difference / (1000 * 3600 * 24));
+    return diffDays;
   };
+
+  // https://stackoverflow.com/questions/1643320/get-month-name-from-date
+  const month = today.toLocaleString("default", { month: "long" });
 </script>
 
-<button>
-  {days(nextSunday, today)}
-</button>
+<main>
+  <p>{month}, {today.getDate()}</p>
+  <button>
+    {days(nextSunday, today)}
+  </button>
 
-<button>
-  {days(nextYear, today)}
-</button>
+  <button>
+    {days(nextYear, today)}
+  </button>
 
-<button>
-  {days(die, today)}
-</button>
+  <button>
+    {days(date60, today)}
+  </button>
+</main>
